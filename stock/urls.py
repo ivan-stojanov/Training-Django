d338669d9d77711bd300d60db9ecaf7965187cfa
Ivan Stojanov <ivan.s@scopicsoftware.com>
2016-06-05 13:34:32 +0200

    complete details page + create blank pages for edit and create

--- stock/urls.py
+++ stock/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [ 
     url(r'^$', views.inventory_list,
         name='list'),
+    url(r'inventory/(?P<pk>\d+)/$', views.inventory_details, 
+        name='details'),
+    url(r'inventory/edit/(?P<pk>\d+)/$', views.inventory_edit, 
+        name='edit'),
+    url(r'inventory/create/$', views.inventory_create, 
+        name='create'),              
     url(r'dashboard/$', views.dashboard,
         name='dashboard'),
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from . import models
 
@@ -10,6 +10,32 @@ def inventory_list(request):
     return render(request, 'stock/inventory_list.html', {
         'inventories': inventories
     })
+    
+    
+def inventory_details(request, pk):
+    inventory = get_object_or_404(
+         models.Inventory,
+         pk=pk,
+         is_available=True
+    )
+    return render(request, 'stock/inventory_details.html', {
+        'inventory': inventory
+    })
+    
+    
+def inventory_edit(request, pk):
+    inventory = get_object_or_404(
+         models.Inventory,
+         pk=pk,
+         is_available=True
+    )
+    return render(request, 'stock/inventory_edit.html', {
+        'inventory': inventory
+    })
+    
+
+def inventory_create(request):
+    return render(request, 'stock/inventory_create.html')    
 
 
 def dashboard(request):
