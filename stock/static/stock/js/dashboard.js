089ec14c19d761bf28f880731135b5e4d8222f7b
Ivan Stojanov <ivan.s@scopicsoftware.com>
2016-06-07 12:01:01 +0200

    complete dashboard pie chart

--- stock/static/stock/js/dashboard.js
+++ stock/static/stock/js/dashboard.js
@@ -1,48 +1,44 @@
-$(document).ready(function(){
-	
-    $('#container').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-        },
-        title: {
-            text: 'Dashboard panel. Statistics about the stock!'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-        series: [{
-            name: 'In stock',
-            colorByPoint: true,
-            data: [{
-                name: 'Microsoft Internet Explorer',
-                y: 4
-            }, {
-                name: 'Chrome',
-                y: 4
-            }, {
-                name: 'Firefox',
-                y: 4
-            }, {
-                name: 'Safari',
-                y: 4
-            }]
-        }]
-    });
-    
-});
+
+function init_piechart(count_by_type){
+		
+		dataSeries = [];
+		
+		count_by_type.forEach(function(value, index, array) {
+			dataSeries[index] = {};
+			dataSeries[index]["y"] = value.count;
+			dataSeries[index]["name"] = value.itemType__type_name
+		});
+		
+	    $('#container').highcharts({
+	        chart: {
+	            plotBackgroundColor: null,
+	            plotBorderWidth: null,
+	            plotShadow: false,
+	            type: 'pie'
+	        },
+	        title: {
+	            text: 'Dashboard panel. Statistics about the stock!'
+	        },
+	        tooltip: {
+	            pointFormat: '{series.name}: <b>{point.y} item(s)</b>'
+	        },
+	        plotOptions: {
+	            pie: {
+	                allowPointSelect: true,
+	                cursor: 'pointer',
+	                dataLabels: {
+	                    enabled: true,
+	                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+	                    style: {
+	                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+	                    }
+	                }
+	            }
+	        },
+	        series: [{
+	            name: 'In stock',
+	            colorByPoint: true,
+	            data: dataSeries
+	        }]
+	    });
+	}  
